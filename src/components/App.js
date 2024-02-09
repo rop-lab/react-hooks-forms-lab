35e26e6c41ef5590d8b63762018f883c56e57d54
cheruiyotrop <cheruiyot.rop@student.moringaschool.com>
2024-02-09 19:55:39 +0300

    assignment lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ShoppingList from "./ShoppingList";
 import Header from "./Header";
 import itemData from "../data/items";
 
 function App() {
   const [items, setItems] = useState(itemData);
+  const [filteredItems, setFilteredItems] = useState([]);
   const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    setFilteredItems(items.filter(item => item.name.toLowerCase().includes(searchText.toLowerCase())));
+  }, [items]);
 
   function handleDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
 
+  const handleSearchChange = (searchText) => {
+    setSearchText(searchText);
+  }
+
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+      <Filter onSearchChange={handleSearchChange} />
+      <ShoppingList items={filteredItems.length > 0 ? filteredItems : items} />
     </div>
   );
 }
